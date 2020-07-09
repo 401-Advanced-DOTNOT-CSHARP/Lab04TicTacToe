d153e4ace6b20d4b2f59634d1ad5d8d6590ef753
Trevor Stubbs <stubbste@gmail.com>
2020-07-09 14:50:15 -0700

    Setup and ready to work

---
+++ Lab04TicTacToeTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static Lab04TicTacToe.Program;
+
+namespace Lab04TicTacToeTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
