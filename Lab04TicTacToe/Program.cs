ee3891742f1ca0f01a8427101b0438b3bed87e9a
Trevor Stubbs <stubbste@gmail.com>
2020-07-09 14:42:23 -0700

    setup

--- Lab04TicTacToe/Program.cs
+++ Lab04TicTacToe/Program.cs
@@ -8,5 +8,14 @@ namespace Lab04TicTacToe
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+
+        }
+
+
     }
 }
